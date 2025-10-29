a0874457da35e7387e52c3136bbb5e8d5166a667
sher ali shahid <127499729+sherubalti@users.noreply.github.com>
2025-10-29 20:03:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def download(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000,debug=True)
+
